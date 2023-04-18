f2605c4003ff2a972ebb9d99b9b3a2348f120070
Nigel Fijnheer <34651169+Neinno@users.noreply.github.com>
2023-04-18 21:42:10 +0200

    Fixed chat

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const path = require('path')
+const port = process.env.PORT || 3000
 
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const { engine } = require("express-handlebars");
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+const historySize = 50
+let history = []
 
-app.get('/', (req, res, next) => {
-    res.render('home', {
-    
+// Serveer client-side bestanden
+app.use(express.static(path.resolve('public')))
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  io.emit('history', history)
+
+  socket.on('message', (message) => {
+    while (history.length > historySize) {
+      history.shift()
+    }
+    history.push(message)
+
+    io.emit('message', socket.nickname + ": " + message);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+io.on('connection', (socket) => {
+    socket.on('send-nickname', (nickname) => {
+        socket.nickname = nickname;
+        io.emit("send-nickname", socket.nickname);
     });
 });
+
+http.listen(port, () => {
+  console.log('listening on http://localhost:' + port)
+})
+
+
---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+let socket = io()
+let messages = document.querySelector('section ul')
+let input = document.querySelector('input')
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (input.value) {
+    socket.emit('message', input.value)
+    input.value = ''
+  }
+})
+
+socket.on('message', (message) => {
+  addMessage(message)
+})
+
+socket.on('history', (history) => {
+  history.forEach((message) => {
+    addMessage(message)
+  })
+})
+
+function username() {
+    let text;
+    let nickname = prompt("Name:", "");
+    if (nickname == null || nickname == "") {
+        text = "User cancelled the prompt.";
+    } else {
+        socket.emit('send-nickname', nickname);
+    }
+}
+
+username();
+
+function addMessage(message) {
+  messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
+  messages.scrollTop = messages.scrollHeight
+}
